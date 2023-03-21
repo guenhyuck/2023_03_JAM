8b0a25a2f0e3390191bfb13304f6c17264b055d2
guenhyuck <guenhyuck94@gmail.com>
2023-03-21 15:09:23 +0900

    게시물 리스팅 시 데이터를 DB로부터 가져오기

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -1,7 +1,13 @@
 package com.KoreaIT.example.JAM;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 import java.util.Scanner;
 
 public class Main {
@@ -9,17 +15,15 @@ public class Main {
 		System.out.println("=프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
 
-		List<Article> articles = new ArrayList<>();
 		int lastArticleId = 0;
 
 		while (true) {
+
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
-
 			if (cmd.equals("exit")) {
 				break;
 			}
-
 			if (cmd.equals("article write")) {
 				System.out.println("==게시물 작성==");
 				int id = lastArticleId + 1;
@@ -29,27 +33,126 @@ public class Main {
 				String body = sc.nextLine();
 
 				Article article = new Article(id, title, body);
-				articles.add(article);
 
 				System.out.println(id + "번 글이 생성되었습니다");
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW(),";
+					sql += "updateDate = NOW(),";
+					sql += "title = '" + title + "',";
+					sql += "`body` = '" + body + "';";
+					System.out.println(sql);
+					pstmt = conn.prepareStatement(sql);
+					int affectedRow = pstmt.executeUpdate();
+					System.out.println("affectedRow : " + affectedRow);
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 				lastArticleId++;
 			} else if (cmd.equals("article list")) {
 				System.out.println("==게시물 목록==");
 
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+
+				List<Article> articles = new ArrayList<>();
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT *";
+					sql += " FROM article";
+					sql += " ORDER BY id DESC;";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+
+					rs = pstmt.executeQuery(sql);
+
+					while (rs.next()) {
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+					}
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+				}
+
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
 					continue;
-				}
 
+				}
 				System.out.println("번호   /   제목");
 
 				for (Article article : articles) {
-					System.out.printf("%d   /   %s\n",article.id, article.title);
+					System.out.printf("%4d   /   %s\n", article.id, article.title);
 				}
 			}
 
 		}
-
 		System.out.println("==프로그램 종료==");
 		sc.close();
 	}
--- src/com/KoreaIT/example/JAM/test/JDBCModifyTest.java
+++ src/com/KoreaIT/example/JAM/test/JDBCModifyTest.java
@@ -3,12 +3,20 @@ package com.KoreaIT.example.JAM.test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class JDBCInsertTest {
+import com.KoreaIT.example.JAM.Article;
+
+public class JDBCModifyTest {
 	public static void main(String[] args) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		List<Article> articles = new ArrayList<>();
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -17,19 +25,26 @@ public class JDBCInsertTest {
 			conn = DriverManager.getConnection(url, "root", "");
 			System.out.println("연결 성공!");
 
-			String sql = "INSERT INTO article";
-			sql += " SET regDate = NOW(),";
-			sql += "updateDate = NOW(),";
-			sql += "title = CONCAT('제목 ',RAND()),";
-			sql += "`body` = CONCAT('내용 ',RAND());";
+			String sql = "UPDATE article";
+			sql += " SET id ";
+			sql += " WHERE id = ;";
 
 			System.out.println(sql);
 
 			pstmt = conn.prepareStatement(sql);
 
-			int affectedRow = pstmt.executeUpdate();
+			rs = pstmt.executeQuery(sql);
 
-			System.out.println("affectedRow : " + affectedRow);
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String regDate = rs.getString("regDate");
+				String updateDate = rs.getString("updateDate");
+				String title = rs.getString("title");
+				String body = rs.getString("body");
+
+				Article article = new Article(id, regDate, updateDate, title, body);
+				articles.add(article);
+			}
 
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
@@ -37,12 +52,13 @@ public class JDBCInsertTest {
 			System.out.println("에러 : " + e);
 		} finally {
 			try {
-				if (conn != null && !conn.isClosed()) {
-					conn.close();
+				if (rs != null && !rs.isClosed()) {
+					rs.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+
 			try {
 				if (pstmt != null && !pstmt.isClosed()) {
 					pstmt.close();
@@ -50,7 +66,17 @@ public class JDBCInsertTest {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
 		}
+
+		System.out.println("실행 결과 : " + articles);
 
 	}
 }
