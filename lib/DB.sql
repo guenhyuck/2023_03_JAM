7de4610a05bf2828bbbcdb3b359c78ae6386697f
guenhyuck <guenhyuck94@gmail.com>
2023-03-22 17:55:51 +0900

    회원가능 기능 구현중, 비밀번호 체크, 필수입력

--- lib/DB.sql
+++ lib/DB.sql
@@ -1,9 +1,11 @@
+#DB 생성
+
 DROP DATABASE IF EXISTS `JAM`;
 
 CREATE DATABASE `JAM`;
 
 USE `JAM`;
-
+#article 테이블 생성
 CREATE TABLE article (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
@@ -12,21 +14,43 @@ CREATE TABLE article (
     `body` TEXT NOT NULL
 );
 
+#member 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(200) NOT NULL,
+    `name` CHAR(100) NOT NULL
+);
+
 DROP TABLE article;
 
 SHOW TABLES;
 DESC article;
 
+
+#article 테스트 데이터 생성
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = CONCAT('제목',RAND()),
 `body` =CONCAT('내용',RAND());
 
+#member 테스트 데이터 생성
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = CONCAT('Testid',RAND()),
+loginPw = CONCAT('TestPw',RAND()),
+`name` = CONCAT('Testid',RAND());
+
+SELECT *
+FROM `member`;
+
 SELECT *
 FROM article;
 
-UPDATE article
-SET updateDate = NOW(),
-title = '제목2'
-WHERE id = 2;
+
+
+
